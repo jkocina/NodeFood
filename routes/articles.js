c6b299c3d4fef9872488a1d56dd8abb04fb7644d
Jacob Kocina <Jkocina.jr@gmail.com>
2019-12-06 20:35:59 -0600

    added views for add_article and layout with some formatting

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 
 router.get('/',  (req, res, next) => {
   res.send('articles');
-})
+});
+
+router.get('/articles/add', () => {
+  res.render('add_article', {title: 'Create Article'});
+});
 
 module.exports = router;
